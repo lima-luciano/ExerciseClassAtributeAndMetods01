b969d4af6e0969ce0a56988f412809952e3bb8a0
Devllu <dev.limaluciano@gmail.com>
2021-12-09 02:33:12 -0300

    Add project files.

---
+++ ExerciseClassAtributeAndMetods01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciseClassAtributeAndMetods01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle x = new Rectangle();
+            
+            Console.WriteLine("Type the width and height of rectangle: ");
+            x.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Heigth = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(x.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(x.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(x.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ ExerciseClassAtributeAndMetods01/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciseClassAtributeAndMetods01
+{
+    class Rectangle
+    {
+
+        public double Width;
+        public double Heigth;
+        public double Area()
+        {
+            return Width * Heigth;
+        }
+
+        public double Perimeter()
+        {
+            return (2 * Width) + (2 * Heigth); 
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Math.Pow(Width, 2.0)) + (Math.Pow(Heigth, 2.0)));
+        }
+
+    }
+}
